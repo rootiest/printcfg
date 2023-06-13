204416d0b812765c39a44b68af1c9adfcf745253
Chris <chris@rootiest.com>
2023-06-13 10:05:45 -0400

    fuck clones

--- src/gen_service.py
+++ src/gen_service.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with printcfg.  If not, see <http://www.gnu.org/licenses/>.
 
-# This script generates a service file for printcfg.
-
 """Generate a service file for printcfg."""
 import os
 import sys
